a5fbf9ab4b2b0b84ccd5ca118ebb1a29aa139a97
along88 <a.long88@yahoo.com>
2017-03-08 10:34:56 -0500

    Initial Commit
    WPF SLN

---
+++ RecipeDB/RecipeDB/Commands/ReturnRecipeCmd.cs
@@ -0,0 +1,35 @@
+﻿using RecipeDB.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RecipeDB.Commands
+{
+    class ReturnRecipeCmd : ICommand
+    {
+        private ReturnRecipeViewModel returnRecipeViewModel;
+
+        public ReturnRecipeCmd(ReturnRecipeViewModel returnRecipeViewModel)
+        {
+            this.returnRecipeViewModel = returnRecipeViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return returnRecipeViewModel.UpdateList;
+        }
+        public void Execute(object parameter)
+        {
+            returnRecipeViewModel.RecipeRequested();
+        }
+    }
+}
---
+++ RecipeDB/RecipeDB/Models/ReturnRecipeModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace RecipeDB.Models
+{
+    class ReturnRecipeModel : INotifyPropertyChanged
+    {
+        private string recipeName;
+        DataTable recipeList;
+
+        public string RecipeName
+        {
+            get => recipeName;
+            set
+            {
+                recipeName = value;
+                OnPropertyChange(RecipeName);
+            }
+        }
+        public DataTable RecipeList
+        {
+            get
+            {
+                return recipeList;
+            }
+            set
+            {
+                recipeList = value;
+            }
+                
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<EventArgs> OnRequest;
+
+        private void OnPropertyChange(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
