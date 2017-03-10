d4a2d2df8f5bd72c799519d045baa36a1b142845
along88 <a.long88@yahoo.com>
2017-03-10 12:17:40 -0500

    added more columns

--- RecipeDB/RecipeDB/Models/ReturnRecipeModel.cs
+++ RecipeDB/RecipeDB/Models/ReturnRecipeModel.cs
@@ -33,10 +33,17 @@ namespace RecipeDB.Models
                     recipeData.Tables.Add("Recipes");
                     recipeData.Tables[0].Columns.Add("Recipe");
                     recipeData.Tables[0].Columns.Add("Type");
+                    recipeData.Tables[0].Columns.Add("Effect");
+                    recipeData.Tables[0].Columns.Add("Description");
+                    
+
 
 
                 DataRow dr = recipeData.Tables[0].NewRow();
                 dr["Recipe"] = recipeName;
+                dr["Type"] = "Dish";
+                dr["Effect"] = "restores three quarters of a heart";
+                dr["Description"] = "Direct heat has softened and sweetened this Apple. Eat it to restore three quarters of a heart.";
 
                 recipeData.Tables[0].Rows.Add(dr);
                 return recipeData.Tables[0].DefaultView;
@@ -44,6 +51,7 @@ namespace RecipeDB.Models
             set
             {
                 recipeList = value;
+                OnPropertyChanged("RecipeList");
             }
                 
         }
